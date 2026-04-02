d354360253f044d8bac53ce16a8c758ded18d0a3
saahiyo <linkg518@gmail.com>
2026-04-02 23:46:48 +0530

    chore: update .gitignore to include .env files, remove metadata.json, and adjust imports in configuration files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: ['http://localhost:3000', '172.29.96.1'],
 };
 
 export default nextConfig;
